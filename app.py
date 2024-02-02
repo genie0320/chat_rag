4b227a88f09051e3deb8f0ff1f468bedd1d3144e
Genie Park <genie0320@gmail.com>
2024-02-03 00:01:09 +0900

    _ add vector store

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-# !pip install langchain langchain-openai streamlit
+# !pip install langchain langchain-openai streamlit beautifulsoup4 chroma
 # !pip install Jupytext # Need only in jupyter env. then run 'jupytext --to py app.ipynb'
 
+from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+
+load_dotenv()
 
 
 def get_response(user_input):
     return "Loading..."
+
+
+def get_vectorstore_from_url(url):
+    # get the text in the website
+    loader = WebBaseLoader(url)
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(documents)
+
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    return vector_store
 
 
 # App config
@@ -23,11 +42,21 @@ if "chat_history" not in st.session_state:
 # Sidebar
 with st.sidebar:
     st.header("settings")
-    website_url = st.text_input("website URL")
+    website_url = st.text_input("Input website URL")
+
+"""
+https://python.langchain.com/docs/get_started/introduction
+"""
 
 if website_url is None or website_url == "":
     st.info("웹사이트 주소를 입력해주세요.")
 else:
+    document_chunks = get_vectorstore_from_url(website_url)
+
+    # For debug
+    # with st.sidebar:
+    #     st.write(document_chunks)
+
     # initial setting
     user_input = st.chat_input("무엇이 궁금하신가요?")
 
@@ -37,6 +66,7 @@ else:
         st.session_state.chat_history.append(HumanMessage(content=user_input))
         st.session_state.chat_history.append(AIMessage(content=response))
 
+    # For debug
     # with st.sidebar:
     #     st.write(st.session_state.chat_history)
 
