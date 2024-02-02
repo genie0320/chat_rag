2db078823933ed971f67e75b8f8c9b15389166b6
Genie Park <genie0320@gmail.com>
2024-02-02 23:18:30 +0900

    _add chat history

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # !pip install Jupytext # Need only in jupyter env. then run 'jupytext --to py app.ipynb'
 
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
 
 def get_response(user_input):
@@ -12,20 +13,38 @@ def get_response(user_input):
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with website")
 
+
+# chat_history가 variable이 아니라 str로 들어간다는 것에 주의.
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="{$site.title} 입니다. 무엇이 궁금하신가요?"),
+    ]
+
 # Sidebar
 with st.sidebar:
     st.header("settings")
     website_url = st.text_input("website URL")
 
-# initial setting
-user_input = st.chat_input("무엇이 궁금하신가요?")
-with st.chat_message("AI"):
-    st.write("What can I help you?")
+if website_url is None or website_url == "":
+    st.info("웹사이트 주소를 입력해주세요.")
+else:
+    # initial setting
+    user_input = st.chat_input("무엇이 궁금하신가요?")
 
-# chat ui
-if user_input is not None and user_input != "":
-    response = get_response(user_input)
-    with st.chat_message("human"):
-        st.write(user_input)
-    with st.chat_message("AI"):
-        st.write(response)
+    # chat ui
+    if user_input is not None and user_input != "":
+        response = get_response(user_input)
+        st.session_state.chat_history.append(HumanMessage(content=user_input))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    # with st.sidebar:
+    #     st.write(st.session_state.chat_history)
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("human"):
+                st.write(message.content)
